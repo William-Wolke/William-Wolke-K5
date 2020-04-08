4692814debcc2e3884ec263448d90da9ee7f2afb
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-04-08 20:13:41 +0200

    Mycket nytt
    Jag struntade i att ha php script men det ser fint ut iallafall. Fixade så att sidan har olika layouts beroende på om man är på mobil eller inte. Skrev javascript som bara kördes ifall man var på desktop. Den kollar hur bred skärmen är. Lade in fina bilder och lade in många små style touches. Lade in mina gamla projekt som bilder och länkade de i nav. Jag satte navbar på andar sidan och bestämmde mig för ta bort effekten då det inte passade på sidan som jag gör :( det det vore coolt med effekter.

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let homeButton = document.getElementById("homeButton");
+let contactButton = document.getElementById("contactButton");
+let projectsButton = document.getElementById("projectsButton");
+
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+let hero = document.getElementById('hero');
+let projects = document.getElementById('projects');
+let contact = document.getElementById('contact');
+
+homeButton.addEventListener("click", checkHomeButton);
+contactButton.addEventListener("click", checkContactButton);
+projectsButton.addEventListener("click", checkProjectsButton);
+
+console.log(heightOutput);
+
+function checkHomeButton() {
+    let w = window.innerWidth;
+
+    if (w >= 700) {
+        hero.style.display = "block";
+        projects.style.display = "none";
+        contact.style.display = "none";
+    }
+}
+
+function checkContactButton() {
+    let w = window.innerWidth;
+
+    if (w >= 700) {
+        hero.style.display = "none";
+        projects.style.display = "block";
+        contact.style.display = "none";
+    }
+}
+
+function checkProjectsButton() {
+    let w = window.innerWidth;
+
+    if (w >= 700) {
+        hero.style.display = "none";
+        projects.style.display = "none";
+        contact.style.display = "block";
+    }
+}
